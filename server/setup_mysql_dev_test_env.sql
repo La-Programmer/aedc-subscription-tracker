4ae4a3517da96354bb1bbf71523d673200808c66
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-07-04 10:36:42 +0100

    Remove setup sql files

--- server/setup_mysql_dev_test_env.sql
+++
@@ -1,6 +0,0 @@
--- Prepares a MySQL server for the project--
-CREATE DATABASE IF NOT EXISTS aedc_subscription_tracker;
-CREATE USER IF NOT EXISTS 'aedc_test_env_user'@'127.0.0.1' IDENTIFIED BY 'aedc@1**23&4';
-GRANT ALL PRIVILEGES ON `aedc_subscription_tracker`.* TO 'aedc_test_env_user'@'127.0.0.1';
-GRANT SELECT ON `performance_schema`.* TO 'aedc_test_env_user'@'127.0.0.1';
-FLUSH PRIVILEGES;
