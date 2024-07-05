8f1895e78d96c09b4083bd68205ffb3b522051e0
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-07-05 11:33:24 +0100

    Stash file

--- server/models/engine/db_storage.py
+++ server/models/engine/db_storage.py
@@ -29,7 +29,10 @@ class DBStorage:
     HOST=os.environ.get("HOST")
     DB=os.environ.get("DB") 
     # print(f'{USER}, {PASSWORD}, {HOST}, {DB}')
-    self.__engine = create_engine(f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:3306/{DB}")    
+    try:
+      self.__engine = create_engine(f"mysql+mysqldb://{USER}:{PASSWORD}@{HOST}:3306/{DB}")
+    except Exception as e:
+      print(e) 
 
   def all(self, cls=None):
     """ Gets all objects of a specific class, or all classes """
